a36dad68866066a490b7c2f47c67ac15c5ce4893
Bhargav1996 <bhargavn1950@gmail.com>
2024-06-18 16:16:30 +0530

    Updated

---
+++ 01basicract/src/App.js
@@ -0,0 +1,53 @@
+
+function App() {
+  return (
+    <h1>Starting React | Bhargav
+      Npx is a node pakage executor
+Create-Reqact-app is a utility like one softwore then give project Name
+
+npx create-react-app
+
+using vite we can create custom Project.
+
+How read react project and packages 
+first open package.json file its a entry point. and check project name version and dependency.
+React and React-dom is a main libreay
+web-vitals checking how your website performe.
+
+// Package.json file inside this file we will see all the dependency, vserion script etc
+
+Script is a actually run a project or ready for production.
+
+Strat : Dev enviroment run.
+Build : Production envirmment
+test : to run all test cases
+eject: 
+
+To run project we hvae to Strat Script 
+Start Script npm run start
+
+When we upload ur application or web build folder will run and serve to user 
+
+// Using Vite create project
+
+Use command:  npm create vite
+Give Project name
+select Framework
+select vairant
+
+This not give you node modules for that we have to manually install node modules  using npm install command
+for run command "npm run dev"
+DevDependence is use for only devlopment.
+
+// SRC Folder
+
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+
+    </h1>
+  );
+}
+
+export default App;
